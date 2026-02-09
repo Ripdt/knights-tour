9d0993de6f2cb54c040e94d7d75485ddeaae866e
Filipi da Costa <89110686+Ripdt@users.noreply.github.com>
2026-02-09 04:34:46 +0000

    faster brute-force

--- brute.cpp
+++ brute.cpp
@@ -1,108 +1,56 @@
+#include <iostream>
+#include <chrono>
+#include <algorithm>
 
 #include "chessboard.h"
 
-#include <iostream>
-#include <chrono>
-
-struct Square
-{
-    int line, column;
+const int moves[8][2] = {
+    {2, 1}, {1, 2}, {-1, 2}, {-2, 1},
+    {-2, -1}, {-1, -2}, {1, -2}, {2, -1}
 };
 
-bool isValidLine(const int line) {
-    return line >= 0 && line < N_BOARD_LINE;
-}
-
-bool isValidColumn(const int column) {
-    return column >= 0 && column < N_BOARD_COLUMN;
-}
-
-bool isValidSquare(const Chessboard& board, const int line, const int column) {
-    return isValidLine(line) && isValidColumn(column) &&
-           !board.isVisitedSquare(line, column);
-}
-
-Square* newSquare(const int line, const int column, const Chessboard& board) 
-{
-    if (isValidSquare(board, line, column)) {
-        auto newSqr = new Square;
-        newSqr->line = line; newSqr->column = column;
-        return newSqr;
-    }
-
-    return nullptr;
-}
-
-std::vector<Square*> validSquares(const Square* currentSquare, const Chessboard& board) 
-{
-    std::vector<Square*> valids;
-
-    if (currentSquare == nullptr) {
-        std::cerr << "Error: currentSquare is nullptr." << std::endl;
-        return valids;
-    }
-
-    const int currentLine = currentSquare->line;
-    const int currentColumn = currentSquare->column;
-
-    if (Square* next = newSquare(currentLine + 2, currentColumn + 1, board))
-         valids.push_back(next);
-    
-    if (Square* next = newSquare(currentLine + 2, currentColumn - 1, board))
-         valids.push_back(next);
-    
-    if (Square* next = newSquare(currentLine - 2, currentColumn + 1, board))
-         valids.push_back(next);
-    
-    if (Square* next = newSquare(currentLine - 2, currentColumn - 1, board))
-         valids.push_back(next);
-    
-    if (Square* next = newSquare(currentLine + 1, currentColumn + 2, board))
-         valids.push_back(next);
-    
-    if (Square* next = newSquare(currentLine + 1, currentColumn - 2, board))
-         valids.push_back(next);
-    
-    if (Square* next = newSquare(currentLine - 1, currentColumn + 2, board))
-         valids.push_back(next);
-    
-    if (Square* next = newSquare(currentLine - 1, currentColumn - 2, board))
-         valids.push_back(next);
-
-    return valids;
-}
-
-void tour(Chessboard& board, const Square* currentSquare, int& step) 
-{
-    if (currentSquare == nullptr) {
-        std::cerr << "Error: Current square is nullptr." << std::endl;
-        return;
+bool brute(int x, int y, Chessboard& board, int& step) {
+    if (!board.hasUnvisitedSquare()) {
+        return true;
     }
 
     ++step;
-    board.markVisitedSquare(currentSquare->line, currentSquare->column);
-
-    Chessboard model = board;
-    std::vector<Square*> valids = validSquares(currentSquare, board);
-
-    for (Square* current : valids)
-    {
-        tour(model, current, step);
-        if (!model.hasUnvisitedSquare()) {
-            board = model;
-            break;
-        }
-        else {
-            model = board;
+    std::vector<std::pair<int, int>> available;
+    for (int i = 0; i < 8; i++) {
+        int nextX = x + moves[i][0];
+        int nextY = y + moves[i][1];
+        if (board.isValidSquare(nextX, nextY) && !board.isVisitedSquare(nextX, nextY)) {
+            int distanceToEdge = std::min({nextX, nextY, board.getHeight() - nextX - 1, board.getWidth() - nextY - 1});
+            available.emplace_back(distanceToEdge, i);
         }
     }
-    
-    for (Square* current : valids)
-    {
-        delete current;
+
+    for (const auto& move : available) {
+        const int i = move.second;
+        const int nextX = x + moves[i][0];
+        const int nextY = y + moves[i][1];
+
+        board.markVisitedSquare(nextX, nextY);
+        if (brute(nextX, nextY, board, step)) {
+            return true;
+        }
+        board.unmarkVisitedSquare(nextX, nextY);
     }
+    return false;
 }
 
+void tour(const int initialLine, const int initialColumn, int& step) {
+    Chessboard board;
+
+    board.markVisitedSquare(initialLine, initialColumn);
+    ++step;
+    if (brute(initialLine, initialColumn, board, step)) {
+        std::cout << "Path:" << std::endl;
+        board.printChessboard();
+    } else {
+        std::cout << "Solution does not exist" << std::endl;
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -112,31 +60,17 @@ int main(int argc, char* argv[]) {
 
     const int initialLine = std::atoi(argv[1]);
     const int initialColumn = std::atoi(argv[2]);
-
-    Chessboard board;
-
-    Square* initialSquare = newSquare(initialLine, initialColumn, board);
-
-    if (initialSquare == nullptr) {
-        std::cerr << "Error: Initial square is invalid." << std::endl;
-        return 1;
-    }
-
     int step = 0;
+
     auto start = std::chrono::high_resolution_clock::now();
 
-    tour(board, initialSquare, step);
+    tour(initialLine, initialColumn, step);
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
 
-    std::cout << "Path:" << std::endl;
-    board.printChessboard();
-
     std::cout << std::endl << std::endl << "Execution time: " << duration.count() << " seconds" << std::endl;
     std::cout << "Steps: " << step << std::endl;
-
-    delete initialSquare;
-
+    
     return 0;
 }
